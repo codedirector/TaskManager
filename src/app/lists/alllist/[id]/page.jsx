8208bc7f4aa9ba1156ba18789de3131fb8062a37
codedirector <shobhitsharma102003@gmail.com>
2025-09-28 19:16:25 +0530

    signup page fixed

--- src/app/lists/alllist/[id]/page.jsx
+++ src/app/lists/alllist/[id]/page.jsx
@@ -3,6 +3,6 @@ import TasksUI from "@/Components/TasksUI";
 import { useParams } from "next/navigation"; 
 export default async function task(){ 
   const {id}=useParams(); 
-  console.log(id) 
+  // console.log(id) 
   return <> <TasksUI listId={id}/>  </>
  }
